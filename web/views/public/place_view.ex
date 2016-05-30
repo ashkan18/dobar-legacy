208cdc3be63c3d0c76d6403a24cd6c4cf61af914
ashkan18 <ashkan18@gmail.com>
2016-05-30 18:30:04 -0400

    use live map

--- web/views/public/place_view.ex
+++ web/views/public/place_view.ex
@@ -1,9 +1,12 @@
-require IEx
 defmodule Dobar.Public.PlaceView do
   use Dobar.Web, :view
 
+  def google_static_map_url(%Geo.Point{coordinates: {lat, lon}}, x, y) do
+     "https://maps.googleapis.com/maps/api/staticmap?center=#{lat},#{lon}&markers=color:blue|#{lat},#{lon}&zoom=13&size=#{x}x#{y}&key=AIzaSyBCOj7b6tahJ1qR6zU8XL5xH6KcH5tyMwU"
+  end
+
   def google_map_url(%Geo.Point{coordinates: {lat, lon}}, x, y) do 
-    "https://maps.googleapis.com/maps/api/staticmap?center=#{lat},#{lon}&markers=color:blue|#{lat},#{lon}&zoom=13&size=#{x}x#{y}&key=AIzaSyBCOj7b6tahJ1qR6zU8XL5xH6KcH5tyMwU"
+    "<iframe width=#{x} height=#{y} frameborder=0 style='border:0' src='https://www.google.com/maps/embed/v1/place?center=#{lat},#{lon}&zoom=13&q=#{lat},#{lon}&key=AIzaSyBCOj7b6tahJ1qR6zU8XL5xH6KcH5tyMwU' allowfullscreen></iframe>"
   end
 
   def full_address(place) do
