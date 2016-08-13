f1a9901cbcd08da0400bfaf14fe7951c60c7f6f8
ashkan18 <ashkan18@gmail.com>
2016-08-13 12:01:14 -0400

    reduce km, order by distance

--- web/models/place.ex
+++ web/models/place.ex
@@ -72,9 +72,11 @@ defmodule Dobar.Place do
   Gets places withing distance of a specific location with default of 10km
   4th param is in meters
   """
-  def within_distance(query, lat, lon, distance \\ 10000) do
+  def within_distance(query, lat, lon, distance \\ 7500) do
     point = %Geo.Point{ coordinates: {lat, lon}, srid: 4326}
-    from place in query, where: fragment("ST_DWithin(?::geography, ?::geography, ?)", place.geom, ^point, ^distance)
+    from place in query,
+    where: fragment("ST_DWithin(?::geography, ?::geography, ?)", place.geom, ^point, ^distance),
+    order_by: fragment("ST_Distance(?::geography, ?::geography)", place.geom, ^point)
   end
 
   def paginate(query, page, size) do
